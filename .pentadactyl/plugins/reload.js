745e59aaa490eed9e168dd21b070c9a7c6e15289
kikyo <frederick.zou@gmail.com>
2012-09-09 18:53:35 +0800

    support @import in css rules

--- .pentadactyl/plugins/reload.js
+++ .pentadactyl/plugins/reload.js
@@ -2,7 +2,7 @@
 // @Author:      eric.zou (frederick.zou@gmail.com)
 // @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
 // @Created:     Wed 28 Mar 2012 12:25:52 AM CST
-// @Last Change: Sun 09 Sep 2012 03:44:09 PM CST
+// @Last Change: Sun 09 Sep 2012 06:51:50 PM CST
 // @Revision:    121
 // @Description:
 // @Usage:
@@ -11,6 +11,14 @@
 
 if (window.timertimer)
     window.clearInterval(window.timertimer);
+
+let baseURI = function(location) {
+    let host = location.host;
+    let pathname = location.pathname;
+    let protocol = location.protocol;
+    let base = protocol + '//' + host + pathname;
+    return util.newURI(base);
+};
 
 let exec = function() {
     let doc = window.gBrowser.mCurrentBrowser.contentDocument;
@@ -45,15 +53,17 @@ let checkLocation = function(location, timestamp) {
 let check = function(collection, attr) {
     return function(doc, timestamp) {
         return Array.some(doc[collection], function(item) {
-            let src = item[attr];
-            // if (!src && collection == 'styleSheets') { // @import statement
-                // // @FIXME: (NS_ERROR_MALFORMED_URI) [nsIIOService.newURI]
-                // let checkCssRules = check('cssRules', 'href');
-                // return checkCssRules(item, timestamp);
-            // }
+            let src = item[attr] || false;
+            if (!src && collection == 'styleSheets') { // @import statement
+                // @FIXME: (NS_ERROR_MALFORMED_URI) [nsIIOService.newURI]
+                let checkCssRules = check('cssRules', 'href');
+                return checkCssRules(item, timestamp);
+            }
             if (!src || src.length == 0)
                 return false;
-            let uri = util.newURI(src);
+            let location = window.gBrowser.mCurrentBrowser.contentDocument.location;
+            let _baseURI = baseURI(location);
+            let uri = util.newURI(src, null, _baseURI);
             if (uri.scheme === 'file') {
                 if (typeof item.lastModifiedTime === 'undefined') {
                     let file = io.File(uri.filePath);
@@ -79,7 +89,7 @@ function onUnload() {
 
 // 添加右键菜单，有指定动作
 // inotify
-// 检测 svg images scripts stylesheets
+// 检测 svg images
 // symbolic links???
 // 检测非正常页面，比如下载页面，扩展管理页面
 // vim: set et ts=4 sw=4:
